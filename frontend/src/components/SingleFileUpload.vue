4520dcb83c6d9b9a03871a08f908b6b4f2e1de33
高语越 <gaoyuyue@outlook.com>
2018-06-26 16:12:11 +0800

    修改Uploader.vue

--- frontend/src/components/SingleFileUpload.vue
+++ frontend/src/components/SingleFileUpload.vue
@@ -2,15 +2,15 @@
     <div>
       <uploader
         browse_button="browse_button"
-        start_button="start_button"
         :url="server_config.url+'/File/'"
         :multi_selection="false"
         v-model="files"
         :FilesAdded="filesAdded"
+        @inputUploader="inputUploader"
       />
       <el-button id="browse_button" type="primary">选择文件</el-button>
       <span v-for="file in files">{{file.name}}</span>
-      <el-button id="start_button" type="danger ">开始上传</el-button>
+      <el-button type="danger" @click="up.start()">开始上传</el-button>
     </div>
 </template>
 
@@ -21,7 +21,8 @@
     data() {
       return {
         server_config: this.global.server_config,
-        files:[]
+        files:[],
+        up: {}
       }
     },
     methods: {
@@ -34,6 +35,9 @@
         if (up.files.length > 1) {
           up.removeFile(up.files[0])
         }
+      },
+      inputUploader(up) {
+        this.up = up;
       }
     },
     components: {
--- frontend/src/components/Uploader.vue
+++ frontend/src/components/Uploader.vue
@@ -1,3 +1,4 @@
+<template></template>
 <script>
   import plupload from 'plupload/js/plupload.full.min.js'
   export default {
@@ -7,12 +8,6 @@
         type: Array
       },
       browse_button: {
-        type: String
-      },
-      start_button: {
-        type: String
-      },
-      stop_button: {
         type: String
       },
       container: {
@@ -148,25 +143,25 @@
         type: Function
       }
     },
-    methods: {
-      handle(up) {
-        this.$emit('input', up.files);
-        this.$emit('queueProgress', up.total);
+    data() {
+      return {
+        up: {},
+        files: []
+      }
+    },
+    watch: {
+      files(val) {
+        this.$emit('input', val);
       },
+      up(val) {
+        this.$emit('inputUploader', val);
+      }
+    },
+    methods: {
       init() {
         let that = this;
         const preInitMethod = {
           Init(up, info) {
-            if (that.start_button != null) {
-              document.getElementById(that.start_button).onclick = () => {
-                up.start();
-              };
-            }
-            if (that.stop_button != null) {
-              document.getElementById(that.stop_button).onclick = () => {
-                uploader.stop();
-              }
-            }
             if (that.Init != null) {
               that.Init(up, info);
             }
@@ -189,19 +184,16 @@
             }
           },
           OptionChanged(up, name, value, oldValue) {
-            that.handle(up);
             if (that.OptionChanged != null) {
               that.OptionChanged(up, name, value, oldValue);
             }
           },
           Refresh(up) {
-            that.handle(up);
             if (that.Refresh != null) {
               that.Refresh(up);
             }
           },
           StateChanged(up) {
-            that.handle(up);
             if (that.StateChanged != null) {
               that.StateChanged(up);
             }
@@ -212,19 +204,16 @@
             }
           },
           QueueChanged(up) {
-            that.handle(up);
             if (that.QueueChanged != null) {
               that.QueueChanged(up);
             }
           },
           UploadProgress(up, file) {
-            that.handle(up);
             if (that.UploadProgress != null) {
               that.UploadProgress(up, file);
             }
           },
           FilesRemoved(up, files) {
-            that.handle(up);
             if (that.FilesRemoved != null) {
               that.FilesRemoved(up, files);
             }
@@ -235,31 +224,26 @@
             }
           },
           FilesAdded(up, files) {
-            that.handle(up);
             if (that.FilesAdded != null) {
               that.FilesAdded(up, files);
             }
           },
           FileUploaded(up, file, info) {
-            that.handle(up);
             if (that.FileUploaded != null) {
               that.FileUploaded(up, file, info);
             }
           },
           ChunkUploaded(up, file, info) {
-            that.handle(up);
             if (that.ChunkUploaded != null) {
               that.ChunkUploaded(up, file, info);
             }
           },
           UploadComplete(up, files) {
-            that.handle(up);
             if (that.UploadComplete != null) {
               that.UploadComplete(up, files);
             }
           },
           Error(up, args) {
-            that.handle(up);
             if (that.Error != null) {
               that.Error(up, args);
             }
@@ -302,7 +286,10 @@
         if (this.required_features != null) {
           prop["required_features"] = this.required_features;
         }
-        new plupload.Uploader(prop).init();
+        let uploader = new plupload.Uploader(prop);
+        uploader.init();
+        this.files = uploader.files;
+        this.up = uploader;
       }
     },
     mounted() {
