0f37c3b42526c93123258359040dfec872c1f389
高语越 <gaoyuyue@outlook.com>
2018-06-26 13:55:30 +0800

    增加全局变量

---
+++ frontend/src/Global.vue
@@ -0,0 +1,9 @@
+<script>
+  const server_config = {
+    url: 'http://localhost:8080'
+  };
+  export default {
+    server_config
+  }
+</script>
+
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router/index.js'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import global from './Global'
+
+Vue.prototype.global = global
 
 Vue.use(ElementUI);
 
