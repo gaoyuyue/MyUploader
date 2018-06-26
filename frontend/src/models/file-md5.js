c0665f4713868b28eb887da8b355a573789df5dd
高语越 <gaoyuyue@outlook.com>
2018-06-26 13:57:04 +0800

    引入js-spark-md5.js，封装file-md5.js

---
+++ frontend/src/models/file-md5.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import SparkMD5 from '../plugins/js-spark-md5.js'
+
+export default function (file, callback) {
+  var blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
+    file = file,
+    chunkSize = 2097152,                             // Read in chunks of 2MB
+    chunks = Math.ceil(file.size / chunkSize),
+    currentChunk = 0,
+    spark = new SparkMD5.ArrayBuffer(),
+    fileReader = new FileReader();
+
+  fileReader.onload = function (e) {
+    console.log('read chunk nr', currentChunk + 1, 'of', chunks);
+    spark.append(e.target.result);                   // Append array buffer
+    currentChunk++;
+
+    if (currentChunk < chunks) {
+      loadNext();
+    } else {
+      callback(null, spark.end());
+      console.log('finished loading');
+    }
+  };
+
+  fileReader.onerror = function () {
+    callback('oops, something went wrong.');
+  };
+
+  function loadNext() {
+    var start = currentChunk * chunkSize,
+      end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+
+    fileReader.readAsArrayBuffer(blobSlice.call(file, start, end));
+  }
+
+  loadNext();
+};
+
