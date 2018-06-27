5d755dbb0802e113778383dbed9918d58fe20fc1
高语越 <gaoyuyue@outlook.com>
2018-06-27 13:00:54 +0800

    解决图片预览延迟问题

--- frontend/src/components/PictureUpload.vue
+++ frontend/src/components/PictureUpload.vue
@@ -10,11 +10,11 @@
     <br/>
     <br/>
     <el-row>
-      <el-col :span="4" v-for="(file, index) in files" :key="index" :offset="index > 0 ? 1 : 0">
+      <el-col :span="4" v-for="(image, index) in images" :key="index" :offset="index > 0 ? 1 : 0">
         <el-card :body-style="{ padding: '0px' }">
-          <img v-if="file.imgsrc != null" width="240px" height="240px" :src="file.imgsrc" class="image">
+          <img width="240px" height="240px" :src="image.src" class="image">
           <div style="padding: 14px;">
-              <el-button type="text" class="button" @click="up.removeFile(file)">删除</el-button>
+              <el-button type="text" class="button" @click="deleteFile(index,image.id)">删除</el-button>
           </div>
         </el-card>
       </el-col>
@@ -35,10 +35,21 @@
         server_config: this.global.server_config,
         files:[],
         up: {},
+        images: [],
+        changed: false
       }
     },
     components: {
       'uploader': Uploader
+    },
+    watch: {
+      changed() {
+        let images = [];
+        this.files.forEach((e) => {
+          images.push({src: e.imgsrc, id: e.id});
+        });
+        this.images = images;
+      }
     },
     methods: {
       inputUploader(up) {
@@ -49,29 +60,14 @@
         files.forEach((f) => {
           FileUrl(f.getNative(), (err, imgsrc) => {
             f.imgsrc = imgsrc;
+            this.changed = !this.changed;
           });
-
-          // if (!file || !/image\//.test(file.type)) return; //确保文件是图片
-          // if (file.type == 'image/gif') { //gif使用FileReader进行预览,因为mOxie.Image只支持jpg和png
-          //   var fr = new mOxie.FileReader();
-          //   fr.onload = function () {
-          //     file.imgsrc = fr.result;
-          //     fr.destroy();
-          //     fr = null;
-          //   };
-          //   fr.readAsDataURL(file.getSource());
-          // } else {
-          //   var preloader = new mOxie.Image();
-          //   preloader.onload = function () {
-          //     preloader.downsize(180, 120); //先压缩一下要预览的图片
-          //     var imgsrc = preloader.type == 'image/jpeg' ? preloader.getAsDataURL('image/jpeg', 80) : preloader.getAsDataURL(); //得到图片src,实质为一个base64编码的数据
-          //     file.imgsrc = imgsrc;
-          //     preloader.destroy();
-          //     preloader = null;
-          //   };
-          //   preloader.load(file.getSource());
-          // }
         });
+      },
+      deleteFile(index,id) {
+        this.images.splice(index,1);
+        let file = this.up.getFile(id);
+        this.up.removeFile(file);
       }
     }
   }
